18c45a3328016e3b1c456fe30ff00600f5821c4f
ravi-prakash1907 <1907prakash.ravi@gmail.com>
2019-11-02 04:48:30 +0530

    init

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # TODO-List-App-Flask
+It's my first Flask project
+
+
+This is a todo-list app, developed using **Flask** that lets user to add new tasks that are to be done.
+Also, you can update or delete the tasks, as well!
+
+
+## Requirements
+
+To run this app, we need following:
+
+1. Flask (python)
+2. virtual environment for flask i.e. ```virtualenv```
+3. SQLAlchemy
+
+## Steps to run 
+_Make sure you have all the dependancies installed_
+Open this project directory in the terminal/console
+
+#### Once (only, while running for first time)
+```
+virtualenv env
+source env/bin/activate
+from app import db
+db.create_all()
+exit()
+```
+#### Before opening the browser
+```
+source env/bin/activate
+```
+```
+python3 app.py
+```
---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+####    Dependancies for DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'     #can be 'sqlite:////<path for database>'
+db = SQLAlchemy(app)
+
+#####   Model for DATABASE
+
+class Todo(db.Model):
+    # columns in the table of todo list
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String, nullable=False)
+    #completed = db.Column(db.Integer)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # returns a string, every time as a new element is created
+    def __repr__(self):
+        return '<Task %r>' %self.id
+
+#####   Ends Model
+
+
+
+@app.route('/', methods=['POST', 'GET'])        # methods (a list) = adds 2 methods that app can accept --> GET is default
+def index():
+    # form handling
+    if request.method == 'POST':
+        task_content = request.form['content']   # grab data from form using 'ID-Name'
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)        # adds new_task
+            db.session.commit()             # saves/commits new_task into database
+            return redirect('/')            # loads/redirects to the page i.e. root dir. using redirect()
+        except:
+            return 'Oops!! Something is bad!'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()    # an instance of the database model to query it, all() or first()-><for first data>
+        return render_template('index.html', tasks=tasks)       # not just 'tasks' ---> it'd be:  'tasks=tasks'
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)  # either grabs(if exixts) the task - to be deleted, or 404
+
+    try:
+        db.session.delete(task_to_delete)       # deletes the grabbed task
+        db.session.commit()                     # commits the changes to the database
+        return redirect('/')                    # loads the page i.e. root i.e. /
+    except:
+        return 'Oops!! Error occured while deleting!'
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):    
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']  # there is nothing like --> session.update()
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Oops!! Error occured while updating!'
+    
+    else:
+        return render_template('update.html', task=task)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
